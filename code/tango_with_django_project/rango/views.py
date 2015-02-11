f6e19d1a7e3537ee4e76979312465ad31c38aa48
metigue <2109381e@student.gla.ac.uk>
2015-02-11 05:56:22 +0000

    Chapter 8 - Completed

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -2,6 +2,31 @@ from django.shortcuts import render
 from rango.models import Category
 from django.http import HttpResponse
 from rango.models import Page
+from rango.forms import CategoryForm
+
+def add_category(request):
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
