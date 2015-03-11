84d8a23426304aeaa254daa489faf6732a75dbf5
metigue <metigue@googlemail.com>
2015-03-11 18:25:02 +0000

    Chapter 15 - Completed

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -6,7 +6,22 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from datetime import datetime
+from rango.bing_search import run_query
 
+
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
 
 
 @login_required
